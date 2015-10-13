82a4a59d5583a57c181bee3fca82b2cf213e8020
Julius Jimenez <jjimenez149@student.rcc.edu>
2015-10-12 18:36:17 -0700

    Initial push after first sprint
    First integration of Endless Runner

---
+++ Endless Runner/Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour 
+{
+	public float jumpHeight = 25.0f;
+	public Rigidbody rb;
+
+	public bool isFalling = false;
+
+	public GameObject swordSlashPrefab = null;
+	public Transform leftSpawner = null;
+	public Transform rightSpawner = null;
+
+	public float slashRate;
+	public float nextSlash;
+	
+	// Use this for initialization
+	void Start () 
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetKey(KeyCode.Space) && isFalling == false)
+		{
+			rb.velocity = new Vector3 (0, jumpHeight, 0);
+			isFalling = true;
+		}
+		if (Input.GetKey(KeyCode.A) && Time.time >= nextSlash) 
+		{
+			nextSlash = Time.time + slashRate;
+			Instantiate (swordSlashPrefab, leftSpawner.position, leftSpawner.rotation);
+		}
+		if (Input.GetKey(KeyCode.D) && Time.time >= nextSlash) 
+		{
+			nextSlash = Time.time + slashRate;
+			Instantiate (swordSlashPrefab, rightSpawner.position, rightSpawner.rotation);
+		}
+
+	}
+	void OnCollisionStay (Collision collisionInfo)
+	{
+		isFalling = false;
+	}
+}
